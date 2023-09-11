3ca196fa68c79e74b8ee3e32f78c1a718421a08a
GilsonRodriguesAlmeida <gilson.almeida@al.infnet.edu.br>
2023-09-10 22:14:57 -0300

    Alteração parametros main

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
-import CardGame from "./src/components/CardGame";
+import BoardGame from "./src/objects/BordGame";
 
 //$ boa pratica para uso de recurso de tela
 const $root = document.querySelector("#root");
 
-const $htmlCardGame = CardGame();
+const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
